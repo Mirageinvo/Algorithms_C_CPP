50dadbaf17b5e12785de3b684b553a4c63914389
Mirage <andrej_malinin@mail.ru>
2022-04-24 17:56:02 +0300

    some solved problems added

---
+++ Z_функция_2.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+void mk_z_func(vector<size_t>& z, const string& s) {
+    size_t l = 0, r = 0;
+    for (int i = 1; i < s.size(); ++i) {
+        if (i <= r) {
+            z[i] = min(r - i + 1, z[i - l]);
+        }
+        while (i + z[i] < s.size() && s[z[i]] == s[i + z[i]]) {
+            ++z[i];
+        }
+        if (i + z[i] > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+}
+
+int main() {
+    string s;
+    cin >> s;
+    vector<size_t> z(s.size(), 0);
+    mk_z_func(z, s);
+    for (auto&& el : z) {
+        cout << el << " ";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ Расстояние Левенштейна.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    vector<vector<size_t>> dp(s1.size() + 1, vector<size_t>(s2.size() + 1, 0));
+    for (size_t i = 0; i < dp.size(); ++i) {
+        dp[i][0] = i;
+    }
+    for (size_t i = 0; i < dp[0].size(); ++i) {
+        dp[0][i] = i;
+    }
+    for (size_t i = 1; i < dp.size(); ++i) {
+        for (size_t j = 1; j < dp[i].size(); ++j) {
+            dp[i][j] = min(dp[i - 1][j] + 1, min(dp[i][j - 1] + 1, dp[i - 1][j - 1] + (s1[i] == s2[j] ? 0 : 1)));
+        }
+    }
+    cout << dp[s1.size()][s2.size()];
+    return 0;
+}
