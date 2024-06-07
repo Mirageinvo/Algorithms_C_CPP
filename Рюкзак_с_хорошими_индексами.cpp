b6a0d724c4ab079939ddc2404d29ffd9c6e28e63
Malinin Andrey <andrej_malinin@mail.ru>
2024-06-08 01:20:30 +0300

    Backpack with better indexes

---
+++ Рюкзак_с_хорошими_индексами.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <set>
+#include <cstdio>
+#include <map>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+using namespace std;
+
+const long int INF = 1e9;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> w(N + 1);
+    vector<int> c(N + 1);
+    for (int i = 1; i <= N; ++i) {
+        cin >> w[i];
+    }
+    for (int i = 1; i <= N; ++i) {
+        cin >> c[i];
+    }
+    vector<vector<int>> dp(N + 1, vector<int>(M + 1, 0));
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 0; j <= M; ++j) {
+            dp[i][j] = dp[i - 1][j];
+            if (w[i] <= j) {
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + c[i]);
+            }
+        }
+    }
+    int ans = -1;
+    for (int i = 0; i <= M; ++i) {
+        ans = max(ans, dp[N][i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
