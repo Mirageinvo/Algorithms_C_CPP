e7a882cde276be752740cd7cd9ecd5f1dd2e1e77
Malinin Andrey <andrej_malinin@mail.ru>
2024-07-03 00:32:15 +0300

    Added Heap for min and max and added heap sort

---
+++ HeapMax(куча на максимум).cpp
@@ -0,0 +1,95 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <set>
+#include <cstdio>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+using namespace std;
+
+const long int INF = 1e9;
+
+template<typename T>
+class HeapMax {
+public:
+    void Add(int el) {
+        arr.push_back(el);
+        int cur = arr.size() - 1;
+        int parent = (cur - 1) / 2;
+        while (cur > 0 && arr[cur] > arr[parent]) {
+            swap(arr[cur], arr[parent]);
+            cur = parent;
+            parent = (cur - 1) / 2;
+        }
+    }
+
+    void Heapify(int i) {
+        int left, right, largestChild;
+        while (true) {
+            left = i * 2 + 1;
+            right = i * 2 + 2;
+            largestChild = i;
+
+            if (left < arr.size() && arr[left] > arr[largestChild]) {
+                largestChild = left;
+            }
+            if (right < arr.size() && arr[right] > arr[largestChild]) {
+                largestChild = right;
+            }
+
+            if (largestChild == i) {
+                break;
+            }
+
+            swap(arr[i], arr[largestChild]);
+            i = largestChild;
+        }
+    }
+
+    T GetAndRemoveMax() {
+        T ret = arr[0];
+        swap(arr[0], arr[arr.size() - 1]);
+        arr.pop_back();
+        Heapify(0);
+        return ret;
+    }
+
+    HeapMax<T> BuildHeapOnArray(vector<T>&& tmp_arr) {
+        move(arr, tmp_arr);
+        for (int i = (arr.size() - 1) / 2; i >= 0; --i) {
+            Heapify(i);
+        }
+    }
+
+private:
+    vector<T> arr;
+};
+
+int main() {
+    HeapMax<int> heap;
+    int n, type, val;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> type;
+        if (type == 0) {
+            cin >> val;
+            heap.Add(val);
+        }
+        else {
+            cout << heap.GetAndRemoveMax() << endl;
+        }
+    }
+    return 0;
+}
---
+++ HeapMin(куча на минимум с heap sort).cpp
@@ -0,0 +1,92 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <set>
+#include <cstdio>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+using namespace std;
+
+const long int INF = 1e9;
+
+template<typename T>
+class HeapMin {
+public:
+    HeapMin<T>(vector<T>&& tmp_arr) { // Build heap on array
+        swap(arr, tmp_arr);
+        for (int i = (arr.size() - 1) / 2; i >= 0; --i) {
+            Heapify(i);
+        }
+    }
+
+    void Add(int el) {
+        arr.push_back(el);
+        int cur = arr.size() - 1;
+        int parent = (cur - 1) / 2;
+        while (cur > 0 && arr[cur] < arr[parent]) {
+            swap(arr[cur], arr[parent]);
+            cur = parent;
+            parent = (cur - 1) / 2;
+        }
+    }
+
+    void Heapify(int i) {
+        int left, right, smallestChild;
+        while (true) {
+            left = i * 2 + 1;
+            right = i * 2 + 2;
+            smallestChild = i;
+
+            if (left < arr.size() && arr[left] < arr[smallestChild]) {
+                smallestChild = left;
+            }
+            if (right < arr.size() && arr[right] < arr[smallestChild]) {
+                smallestChild = right;
+            }
+
+            if (smallestChild == i) {
+                break;
+            }
+
+            swap(arr[i], arr[smallestChild]);
+            i = smallestChild;
+        }
+    }
+
+    T GetAndRemoveMin() {
+        T ret = arr[0];
+        swap(arr[0], arr[arr.size() - 1]);
+        arr.pop_back();
+        Heapify(0);
+        return ret;
+    }
+
+private:
+    vector<T> arr;
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    HeapMin<int> heap(std::move(v));
+    for (int i = 0; i < n; ++i) {
+        cout << heap.GetAndRemoveMin() << " ";
+    }
+    return 0;
+}
